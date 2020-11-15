d2222e44bd4ecaf90bcfabea11fd5f31f2dbf3d2
HTHQ1234 <851332710@qq.com>
2020-11-15 21:27:54 +0800

    组件化开发

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    counter:0,
+    isShow:true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleIncrement(event){
+    console.log(event)
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTabClick(event){
+    console.log(event.detail.item + event.detail.index)
   },
+  handleIncrementCpn(){
+    // 最终目的：修改my-sel中的counter
+    // 1.获取组件对象
+    const my_sel=this.selectComponent('#sel-id');
+    console.log(my_sel)
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    // 2.通过setData修改组件中的数据
+    my_sel.incrementCounter(10);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChangeShow(){
+    this.setData({
+      isShow:!this.data.isShow
+    })
   }
 })
